7d176f1baf4f464c46c3801f13d9e29aa59e3aee
Kumar Adarsh <becool.adarsh@gmail.com>
2021-05-22 01:29:15 +0530

    update our menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './components/pages/homepages/homepage.component';
 
+const Covid19Essential = () => (
+
+  <div>
+    <h1> COVID19 ESSENTIAL </h1>
+  </div>
+)
+
 function App() {
   return( 
     <div> 
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/Covid19' component={Covid19Essential} />    
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.compnent.jsx
+++ src/components/directory/directory.compnent.jsx
@@ -14,31 +14,36 @@ class Directory extends React.Component {
                     title: 'covid19 essential',
                     imageUrl:'https://images.pexels.com/photos/3951628/pexels-photo-3951628.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940.png',
                     size:'medium',
-                    id:1
+                    id:1,
+                    linkUrl: 'covid19essential'
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://images.pexels.com/photos/3998647/pexels-photo-3998647.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940.png',
                     size: 'medium',
-                    id:2
+                    id:2,
+                    linkUrl: ''
                 },
                 {
                     title: 'shoes',
                     imageUrl: 'https://images.pexels.com/photos/1032110/pexels-photo-1032110.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940.png',
                     size: 'medium',
-                    id:3
+                    id:3,
+                    linkUrl: ''
                 },
                 {
                     title: 'womens',
                     imageUrl: 'https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940.png',
                     size: 'large',
-                    id:4
+                    id:4,
+                    linkUrl: ''
                 },
                 {
                     title: 'mens',
                     imageUrl: 'https://images.pexels.com/photos/3766199/pexels-photo-3766199.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940.png',
                     size: 'large',
-                    id:5
+                    id:5,
+                    linkUrl: ''
                 },
             ]
         };
@@ -47,12 +52,11 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 { 
-                  this.state.sections.map(({title, imageUrl, id, size }) => (
-                        <MenuItem key={id} title={title}  imageUrl={imageUrl} size={size} />
+                  this.state.sections.map(({ id, ...otherSectionProps}) => (
+                        <MenuItem key={id} { ...otherSectionProps } />
                     ))
                 }
             </div>
-
         )
     }
 
